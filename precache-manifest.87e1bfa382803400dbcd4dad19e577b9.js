23d0d411a82457c8959f1a7c995cec8effac7b35
sespertg <sespert@gmail.com>
2019-04-25 13:01:53 -0400

    Updates

---
+++ precache-manifest.87e1bfa382803400dbcd4dad19e577b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9f657e7d261a70e6a9a010cc4c3dac",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "afe8f21174d25912967b",
+    "url": "/clicky-game/static/css/main.2fff4fa3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "afe8f21174d25912967b",
+    "url": "/clicky-game/static/js/main.a9ca7ef6.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "84a631ac679735ed4611defb8cae6c87",
+    "url": "/clicky-game/static/media/bootstrap.84a631ac.png"
+  },
+  {
+    "revision": "03e95dc3d590ab9f95c70bda70647665",
+    "url": "/clicky-game/static/media/moment.03e95dc3.png"
+  }
+]);
